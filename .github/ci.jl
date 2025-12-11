09726ddcc8c2404d941787a1d23bbaddb4395e87
Wen Wei Tseng <sosiristseng@gmail.com>
2025-12-11 17:29:47 +0800

    Use IJulia (#187)
    * Use IJulia
    
    * mkpath

--- .github/ci.jl
+++ .github/ci.jl
@@ -1,21 +1,23 @@
-using Literate
+using IJulia
 using JSON
 using Pkg
-
 ENV["GKSwstype"] = "100"
 
-function main(; rmsvg=true)
-    file = get(ENV, "NB", "test.ipynb")
-    cachedir = get(ENV, "NBCACHE", ".cache")
-    nb = if endswith(file, ".jl")
-        run_literate(file; cachedir)
-    elseif endswith(file, ".ipynb")
-        lit = to_literate(file)
-        run_literate(lit; cachedir)
-    else
-        error("$(file) is not a valid notebook file!")
-    end
-    rmsvg && strip_svg(nb)
+function main(;
+    basedir=get(ENV, "DOCDIR", "docs"),
+    cachedir=get(ENV, "NBCACHE", ".cache"),
+    rmsvg=true)
+    nb = get(ENV, "NB", "test.ipynb")
+    IJulia.installkernel("Julia", "--project=@.")
+    # nbconvert command options
+    kernelname = "--ExecutePreprocessor.kernel_name=julia-1.$(VERSION.minor)"
+    execute = ifelse(get(ENV, "ALLOWERRORS", "false") == "true", "--execute --allow-errors", "--execute")
+    timeout = "--ExecutePreprocessor.timeout=" * get(ENV, "TIMEOUT", "-1")
+    nbout = joinpath(abspath(pwd()), cachedir, nb)
+    mkpath(dirname(nbout))
+    cmd = `jupyter nbconvert --to notebook $(execute) $(timeout) $(kernelname) --output $(nbout) $(nb)`
+    run(cmd)
+    rmsvg && strip_svg(nbout)
     return nothing
 end
 
@@ -37,37 +39,6 @@ function strip_svg(ipynb)
     rm(ipynb; force=true)
     write(ipynb, JSON.json(nb, 1))
     @info "Stripped SVG in $(ipynb). The original size is $(Base.format_bytes(oldfilesize)). The new size is $(Base.format_bytes(filesize(ipynb)))."
-    return ipynb
-end
-
-# Convert a Jupyter notebook into a Literate notebook. Adapted from https://github.com/JuliaInterop/NBInclude.jl.
-function to_literate(nbpath; shell_or_help = r"^\s*[;?]")
-    nb = open(JSON.parse, nbpath, "r")
-    jlpath = splitext(nbpath)[1] * ".jl"
-    open(jlpath, "w") do io
-        separator = ""
-        for cell in nb["cells"]
-            if cell["cell_type"] == "code"
-                s = join(cell["source"])
-                isempty(strip(s)) && continue # Jupyter doesn't number empty cells
-                occursin(shell_or_help, s) && continue  # Skip cells with shell and help commands
-                print(io, separator, "#---\n", s)  # Literate code block mark
-                separator = "\n\n"
-            elseif cell["cell_type"] == "markdown"
-                txt = join(cell["source"])
-                print(io, separator, "#===\n", txt, "\n===#")
-                separator = "\n\n"
-            end
-        end
-    end
-    return jlpath
-end
-
-function run_literate(file; cachedir = ".cache")
-    outpath = joinpath(abspath(pwd()), cachedir, dirname(file))
-    mkpath(outpath)
-    ipynb = Literate.notebook(file, dirname(file); mdstrings=true, execute=true)
-    cp(ipynb, joinpath(outpath, basename(ipynb)); force=true)
     return ipynb
 end
 
