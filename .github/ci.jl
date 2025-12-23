0a003c075d85d62a36436c8ab2fa88e053147d4a
Wen Wei Tseng <sosiristseng@gmail.com>
2025-12-23 19:31:10 +0800

    Use literate

--- .github/ci.jl
+++ .github/ci.jl
@@ -1,28 +1,25 @@
-using IJulia
 using JSON
-using Pkg
+using Literate
+
 ENV["GKSwstype"] = "100"
 
-function main(;
-    basedir=get(ENV, "DOCDIR", "docs"),
-    cachedir=get(ENV, "NBCACHE", ".cache"),
-    rmsvg=true)
-    nb = get(ENV, "NB", "test.ipynb")
-    IJulia.installkernel("Julia", "--project=@.")
-    # nbconvert command options
-    kernelname = "--ExecutePreprocessor.kernel_name=julia-1.$(VERSION.minor)"
-    execute = ifelse(get(ENV, "ALLOWERRORS", "false") == "true", "--execute --allow-errors", "--execute")
-    timeout = "--ExecutePreprocessor.timeout=" * get(ENV, "TIMEOUT", "-1")
-    nbout = joinpath(abspath(pwd()), cachedir, nb)
-    mkpath(dirname(nbout))
-    cmd = `jupyter nbconvert --to notebook $(execute) $(timeout) $(kernelname) --output $(nbout) $(nb)`
-    run(cmd)
-    rmsvg && strip_svg(nbout)
+function main(; dopostproc=true)
+    file = get(ENV, "NB", "test.ipynb")
+    cachedir = get(ENV, "NBCACHE", ".cache")
+    nb = if endswith(file, ".jl")
+        run_literate(file; cachedir)
+    elseif endswith(file, ".ipynb")
+        lit = to_literate(file)
+        run_literate(lit; cachedir)
+    else
+        error("$(file) is not a valid notebook file!")
+    end
+    dopostproc && postprocess(nb)
     return nothing
 end
 
-# Strip SVG output from a Jupyter notebook
-function strip_svg(ipynb)
+# Post-process Jupyter notebook
+function postprocess(ipynb)
     oldfilesize = filesize(ipynb)
     nb = open(JSON.parse, ipynb, "r")
     for cell in nb["cells"]
@@ -30,15 +27,54 @@ function strip_svg(ipynb)
         for output in cell["outputs"]
             !haskey(output, "data") && continue
             datadict = output["data"]
+            ## Remove SVG to reduce file size
             if haskey(datadict, "image/png") || haskey(datadict, "image/jpeg")
                 delete!(datadict, "text/html")
                 delete!(datadict, "image/svg+xml")
             end
+            ## Process LaTeX output, wrap in an array if needed
+            if haskey(datadict, "text/latex")
+                latexcode = datadict["text/latex"]
+                if (! (latexcode isa Vector))
+                    datadict["text/latex"] = [latexcode]
+                end
+            end
         end
     end
     rm(ipynb; force=true)
-    write(ipynb, JSON.json(nb, 1))
+    write(ipynb, JSON.json(nb, 2))
     @info "Stripped SVG in $(ipynb). The original size is $(Base.format_bytes(oldfilesize)). The new size is $(Base.format_bytes(filesize(ipynb)))."
+    return ipynb
+end
+
+# Convert a Jupyter notebook into a Literate notebook. Adapted from https://github.com/JuliaInterop/NBInclude.jl.
+function to_literate(nbpath; shell_or_help = r"^\s*[;?]")
+    nb = open(JSON.parse, nbpath, "r")
+    jlpath = splitext(nbpath)[1] * ".jl"
+    open(jlpath, "w") do io
+        separator = ""
+        for cell in nb["cells"]
+            if cell["cell_type"] == "code"
+                s = join(cell["source"])
+                isempty(strip(s)) && continue # Jupyter doesn't number empty cells
+                occursin(shell_or_help, s) && continue  # Skip cells with shell and help commands
+                print(io, separator, "#---\n", s)  # Literate code block mark
+                separator = "\n\n"
+            elseif cell["cell_type"] == "markdown"
+                txt = join(cell["source"])
+                print(io, separator, "#===\n", txt, "\n===#")
+                separator = "\n\n"
+            end
+        end
+    end
+    return jlpath
+end
+
+function run_literate(file; cachedir = ".cache")
+    outpath = joinpath(abspath(pwd()), cachedir, dirname(file))
+    mkpath(outpath)
+    ipynb = Literate.notebook(file, dirname(file); mdstrings=true, execute=true)
+    cp(ipynb, joinpath(outpath, basename(ipynb)); force=true)
     return ipynb
 end
 
